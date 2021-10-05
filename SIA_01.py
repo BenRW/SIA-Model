2bedcbfee6b23d86393d54ee84497302d17cba83
Ben R-W <br2417@ic.ac.uk>
2021-10-05 13:59:37 +0200

    added some more constants and added bedrock function

--- SIA_01.py
+++ SIA_01.py
@@ -1,9 +1,8 @@
 """
 Created on 05/10/2021
 
-@authors: Ben Romero-Wilcock and Jim Regtien
+@authors: Ben Romero-Wilcock, Jim Regtien and Willem Jan van de Berg
 """
-
 
 import numpy as np
 
@@ -13,4 +12,16 @@ rho = 917
 g = 9.81
 fd = 1.9e-24
 fs = 5.7e-20
+
+# Domain
+Ltot = 20000  # total length of the domain [m]
+dx    =   100 # grid size [m]
+
+
+def get_bedrock(xaxis):
+    # here you put in your own equation that defines the bedrock
+    bedrock = 1900. - xaxis*0.05
+    return bedrock
+
+
 
